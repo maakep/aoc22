1b7dde56b48e0b67811cf0ea25af58840cb30dba
Fredrik Schön <schon.fredrik@mail.com>
2022-11-29 15:34:41 +0100

    Advent of advent of code

---
+++ xmas.js
@@ -0,0 +1,47 @@
+var fs = require("fs");
+var arg1 = process.argv[2];
+var arg2 = process.argv[3];
+
+if (arg1 == "g") generate(_0(arg2));
+else runSolution(_0(arg1), arg2);
+
+function runSolution(num1, num2) {
+  var solution = undefined;
+  console.log("");
+  try {
+    solution = require(`./dec${num1}/p${num2}`);
+  } catch (e) {
+    console.log(e);
+    return console.log(
+      `\r\nNo solution found for ./dec${num1}/p${num2} \r\n(ex: 'node xmas 12' for december 1 part 2)\r\n`
+    );
+  }
+
+  var res = solution();
+  console.log(
+    `\n${new Date()
+      .toTimeString()
+      .substr(0, 8)} - dec${num1}/p${num2}.js result:`
+  );
+  console.log(res);
+}
+
+function generate(num) {
+  fs.mkdirSync(`./dec${num}`);
+  fs.writeFileSync(`./dec${num}/input-test.js`, `module.exports = [];`);
+  fs.writeFileSync(`./dec${num}/input.js`, `module.exports = [];`);
+  fs.writeFileSync(
+    `./dec${num}/p1.js`,
+    `const input = process.argv[4] == "t" ? require("./input-test") : require("./input");
+module.exports = () => { return true; }`
+  );
+  fs.writeFileSync(
+    `./dec${num}/p2.js`,
+    `const input = process.argv[4] == "t" ? require("./input-test") : require("./input");
+module.exports = () => { return true; }`
+  );
+}
+
+function _0(num) {
+  return (0 + num).slice(-2);
+}
